50134eac575be2ad632ad3d6ad45566ac897aabc
at-atanasov <at.atanasov@gmail.com>
2025-07-12 17:47:31 +0300

    Building the Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <h1> Static</h1>
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
