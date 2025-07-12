5dccef3962d5c144ef328caace04ae76b0fdacd7
at-atanasov <at.atanasov@gmail.com>
2025-07-12 16:21:41 +0300

    Using CSS Modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
     <div>
-      <h1>WordWise</h1>
+      <h1 className="test">WordWise</h1>
       <PageNav />
+      <AppNav />
+      <Link to="./app">Go to the app</Link>
     </div>
   );
 }
